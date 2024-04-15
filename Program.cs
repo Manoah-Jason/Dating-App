a4866586f6656b3d417af5dc2237119398681d58
Jason Samuel <jasonsamuel95@outlook.com>
2024-04-15 20:07:51 +0530

    JWT

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using DatingApp.Data;
+using DatingApp.Interfaces;
+using DatingApp.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService,TokenService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
---
+++ Services/TokenService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Entities;
+using DatingApp.Interfaces;
+
+namespace DatingApp.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
