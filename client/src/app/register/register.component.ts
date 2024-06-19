d23ee3c2b0bd0a05ba5475e5217f4854f2b675bf
Jason Samuel <jasonsamuel95@outlook.com>
2024-06-19 21:05:39 +0530

    Test

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,7 +2,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators,ValidationErrors  } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators,ValidationErrors, FormBuilder  } from '@angular/forms';
 import { r3JitTypeSourceSpan } from '@angular/compiler';
 @Component({
   selector: 'app-register',
@@ -17,31 +17,28 @@ model:any={}
 
 validationErrors: any;
 
-constructor(private accountService :AccountService,private toastr :ToastrService){}
+constructor(private fb: FormBuilder,private accountService :AccountService,private toastr :ToastrService){}
 
 ngOnInit():void{
 this.initializeForm();
 }
 
 initializeForm() {
-  this.registerForm = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(8)
-    ]),
-    confirmPassword: new FormControl('', [
-      Validators.required,
-      this.matchValues('password')
-    ])
+  this.registerForm = this.fb.group({
+    gender: ['male'],
+    username: ['', Validators.required],
+    knownAs: ['', Validators.required],
+    dateOfBirth: ['', Validators.required],
+    city: ['', Validators.required],
+    country: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+    confirmPassword: ['', [Validators.required, this.matchValues('password')]],
   });
 
   this.registerForm.controls['password'].valueChanges.subscribe(() => {
     this.registerForm.controls['confirmPassword'].updateValueAndValidity();
   });
 }
-
 matchValues(matchTo: string): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
       const match = control.parent?.get(matchTo);
