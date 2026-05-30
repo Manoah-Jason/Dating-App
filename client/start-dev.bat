b5ab1a396b951d64024ea5821a01ece25a5e17a9
Jason Samuel <jasonsamuel95@outlook.com>
2026-05-31 04:41:45 +0530

    Dating app

---
+++ client/start-dev.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=C:\Users\Admin\AppData\Roaming\nvm\v18.17.1;%PATH:C:\Users\Admin\AppData\Roaming\nvm\v14.20.1;=%
+cd /d "C:\Users\Admin\source\repos\Jason\client"
+npm start
---
+++ client/use-node-20.bat
@@ -0,0 +1,6 @@
+@echo off
+set PATH=C:\Users\Admin\AppData\Roaming\nvm\v20.15.1;%PATH%
+echo Switched to Node 20.15.1
+node --version
+npm --version
+pause
